41a50a6cb2632779933cf2dcad7485e51a8eaf51
Sam Reaves <thesamreaves@gmail.com>
2025-11-25 23:53:29 -0500

    Converting TransactionService into interface and implementation

--- transactions-service/src/main/java/com/samreaves/transactions/service/TransactionService.java
+++ transactions-service/src/main/java/com/samreaves/transactions/service/TransactionService.java
@@ -3,33 +3,18 @@ package com.samreaves.transactions.service;
 import com.samreaves.transactions.entity.Transaction;
 import com.samreaves.transactions.entity.Category;
 import com.samreaves.transactions.entity.TransactionType;
-import com.samreaves.transactions.repository.TransactionRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.lang.NonNull;
 import java.util.List;
 
 @Service
-public class TransactionService {
+public interface TransactionService {
 
-    private final TransactionRepository transactionRepository;
+    public Transaction createTransaction(@NonNull Transaction transaction);
 
-    public TransactionService(TransactionRepository transactionRepository) {
-        this.transactionRepository = transactionRepository;
-    }
+    public List<Transaction> getTransactions();
 
-    public Transaction createTransaction(@NonNull Transaction transaction) {
-        return transactionRepository.save(transaction);
-    }
+    public List<Transaction> getTransactionsByCategory(@NonNull Category category);
 
-    public List<Transaction> getTransactions() {
-        return transactionRepository.findAll();
-    }
-
-    public List<Transaction> getTransactionsByCategory(@NonNull Category category) {
-        return transactionRepository.findByCategory(category);
-    }
-
-    public List<Transaction> getTransactionsByType(@NonNull TransactionType type) {
-        return transactionRepository.findByType(type);
-    }
+    public List<Transaction> getTransactionsByType(@NonNull TransactionType type);
 }
