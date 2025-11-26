2c27d6a355bb1f193fdfa9fe573a8c236bb30557
Sam Reaves <thesamreaves@gmail.com>
2025-11-25 21:29:33 -0500

    Committing basic controller

---
+++ transactions-service/src/main/java/com/samreaves/transactions/controller/TransactionController.java
@@ -0,0 +1,25 @@
+package com.samreaves.transactions.controller;
+
+import com.samreaves.transactions.service.TransactionService;
+import com.samreaves.transactions.entity.*;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import java.util.List;
+
+@RestController
+@RequestMapping("/transactions")
+public class TransactionController {
+    private final TransactionService transactionService;
+
+    public TransactionController(TransactionService transactionService) {
+        this.transactionService = transactionService;
+    }
+
+    @GetMapping
+    public List<Transaction> getTransactions() {
+        return transactionService.getTransactions();
+    }
+    
+}
