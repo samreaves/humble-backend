164b5a786fca5b722f0d45c3881831afdb1ce82b
Sam Reaves <thesamreaves@gmail.com>
2025-11-26 00:01:10 -0500

    Removing @Service annotation from TransactionService interface. Renaming .Java file to .java (big oof). Renaming TransactionService constructor to TransactionServiceImpl in TransactionService implementation file

--- transactions-service/src/main/java/com/samreaves/transactions/service/TransactionService.java
+++ transactions-service/src/main/java/com/samreaves/transactions/service/TransactionService.java
@@ -3,11 +3,10 @@ package com.samreaves.transactions.service;
 import com.samreaves.transactions.entity.Transaction;
 import com.samreaves.transactions.entity.Category;
 import com.samreaves.transactions.entity.TransactionType;
-import org.springframework.stereotype.Service;
 import org.springframework.lang.NonNull;
 import java.util.List;
 
-@Service
+
 public interface TransactionService {
 
     public Transaction createTransaction(@NonNull Transaction transaction);
