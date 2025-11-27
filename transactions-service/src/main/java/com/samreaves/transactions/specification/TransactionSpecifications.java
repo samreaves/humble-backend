42ee6523565e42239f18d8f1667c1efe0b0fadf7
Sam Reaves <thesamreaves@gmail.com>
2025-11-26 20:41:38 -0500

    Adding TransactionSpecifications to generate dynamic filtering of Transactions

---
+++ transactions-service/src/main/java/com/samreaves/transactions/specification/TransactionSpecifications.java
@@ -0,0 +1,46 @@
+package com.samreaves.transactions.specification;
+
+import org.springframework.data.jpa.domain.Specification;
+
+import com.samreaves.transactions.entity.Transaction;
+import com.samreaves.transactions.entity.Category;
+import com.samreaves.transactions.entity.TransactionType;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class TransactionSpecifications {
+    public static Specification<Transaction> hasCategory(Category category) {
+        return (root, query, criteriaBuilder) ->
+            category == null
+                ? criteriaBuilder.conjunction()
+                : criteriaBuilder.equal(root.get("category"), category);
+    }
+
+    public static Specification<Transaction> hasType(TransactionType type) {
+        return (root, query, criteriaBuilder) ->
+            type == null
+                ? criteriaBuilder.conjunction()
+                : criteriaBuilder.equal(root.get("type"), type);
+    }
+
+    public static Specification<Transaction> hasAmount(BigDecimal amount) {
+        return (root, query, criteriaBuilder) ->
+            amount == null
+                ? criteriaBuilder.conjunction()
+                : criteriaBuilder.equal(root.get("amount"), amount);
+    }
+
+    public static Specification<Transaction> hasDescription(String description) {
+        return (root, query, criteriaBuilder) ->
+            description == null
+                ? criteriaBuilder.conjunction()
+                : criteriaBuilder.equal(root.get("description"), description);
+    }
+
+    public static Specification<Transaction> hasTimestamp(LocalDateTime timestamp) {
+        return (root, query, criteriaBuilder) ->
+            timestamp == null
+                ? criteriaBuilder.conjunction()
+                : criteriaBuilder.equal(root.get("timestamp"), timestamp);
+    }
+}
