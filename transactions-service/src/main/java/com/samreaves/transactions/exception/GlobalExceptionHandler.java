7642fa4f376347467dadc1dc2c24526d8d053a33
Sam Reaves <thesamreaves@gmail.com>
2025-11-26 20:39:29 -0500

    Adding GlobalExceptionHandler to catch errors

---
+++ transactions-service/src/main/java/com/samreaves/transactions/exception/GlobalExceptionHandler.java
@@ -0,0 +1,38 @@
+package com.samreaves.transactions.exception;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.dao.DataAccessException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.samreaves.transactions.dto.ErrorResponse;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+    private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<ErrorResponse> handleHttpMessageNotReadableError(HttpMessageNotReadableException ex) {
+        logger.error("HTTP message not readable error occurred", ex);
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+            .body(new ErrorResponse("Invalid request body"));
+    }
+
+    @ExceptionHandler(DataAccessException.class)
+    public ResponseEntity<ErrorResponse> handleDatabaseErrors(DataAccessException ex) {
+        logger.error("Database error occurred", ex);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+            .body(new ErrorResponse("Something went wrong"));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ErrorResponse> handleGenericError(Exception ex) {
+        logger.error("Unexpected error occurred", ex);
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+            .body(new ErrorResponse("Something went wrong"));
+    }
+}
