8bd4dfcf085bf4413e4e66c851f5603fec0ea807
Sam Reaves <thesamreaves@gmail.com>
2025-11-26 20:36:30 -0500

    Adding ErrorResponse DTO

---
+++ transactions-service/src/main/java/com/samreaves/transactions/dto/ErrorResponse.java
@@ -0,0 +1,17 @@
+package com.samreaves.transactions.dto;
+
+public class ErrorResponse {
+    public String message;
+
+    public ErrorResponse(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
