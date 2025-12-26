d0d6d8522fcb9b1404fb287c1349417659d28562
Sam Reaves <thesamreaves@gmail.com>
2025-12-26 18:17:09 -0500

    Ensuring the database connection closes after all tests are complete

--- transactions-service/src/test/java/com/samreaves/transactions/BaseIntegrationTest.java
+++ transactions-service/src/test/java/com/samreaves/transactions/BaseIntegrationTest.java
@@ -6,12 +6,14 @@ import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.postgresql.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import org.junit.jupiter.api.AfterAll;
 
 @SpringBootTest
 @Testcontainers
 public abstract class BaseIntegrationTest {
 
     @Container
+    @SuppressWarnings("resource") // Container is closed in @AfterAll cleanupContainer()
     static PostgreSQLContainer postgres = new PostgreSQLContainer("postgres:15-alpine")
         .withReuse(true);
 
@@ -21,5 +23,12 @@ public abstract class BaseIntegrationTest {
         registry.add("spring.datasource.url", postgres::getJdbcUrl);
         registry.add("spring.datasource.username", postgres::getUsername);
         registry.add("spring.datasource.password", postgres::getPassword);
+    }
+
+    @AfterAll
+    static void cleanupContainer() {
+        if (postgres != null) {
+            postgres.close();
+        }
     }
 }
